3cef9d05e8ec138c65917fba494921dd7738ecd8
K Lange <klange@dakko.us>
2021-02-15 21:16:02 +0900

    Add simple repl to krk-sandbox

--- tools/sandbox.c
+++ tools/sandbox.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include <kuroko/kuroko.h>
 #include <kuroko/vm.h>
 #include <kuroko/util.h>
 
+#include "simple-repl.h"
 
 int main(int argc, char * argv[]) {
-	if (argc < 2) {
-		fprintf(stderr, "expected a string to evaluate\n");
-		return 1;
-	}
 	krk_initVM(0);
 
 	krk_tableDelete(&vm.system->fields, OBJECT_VAL(S("module_paths")));
@@ -25,22 +23,26 @@ int main(int argc, char * argv[]) {
 
 	int retval = 0;
 
-	KrkValue result = krk_interpret(argv[1], 0, "<stdin>","<stdin>");
-	if (!IS_NONE(result)) {
-		if (IS_INTEGER(result)) {
-			retval = AS_INTEGER(result);
+	if (argc > 1) {
+		KrkValue result = krk_interpret(argv[1], 0, "<stdin>","<stdin>");
+		if (!IS_NONE(result)) {
+			if (IS_INTEGER(result)) {
+				retval = AS_INTEGER(result);
+			}
+			KrkClass * type = krk_getType(result);
+			if (type->_reprer) {
+				krk_push(result);
+				result = krk_callSimple(OBJECT_VAL(type->_reprer), 1, 0);
+			}
+			if (IS_STRING(result)) {
+				fprintf(stdout, "%s\n", AS_CSTRING(result));
+			}
+		} else if (krk_currentThread.flags & KRK_HAS_EXCEPTION) {
+			krk_dumpTraceback();
+			retval = 1;
 		}
-		KrkClass * type = krk_getType(result);
-		if (type->_reprer) {
-			krk_push(result);
-			result = krk_callSimple(OBJECT_VAL(type->_reprer), 1, 0);
-		}
-		if (IS_STRING(result)) {
-			fprintf(stdout, "%s\n", AS_CSTRING(result));
-		}
-	} else if (krk_currentThread.flags & KRK_HAS_EXCEPTION) {
-		krk_dumpTraceback();
-		retval = 1;
+	} else {
+		runSimpleRepl();
 	}
 
 	krk_freeVM();
