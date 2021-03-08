36b2f405821a309f37f0ff1d700afb38ef7e2e98
K. Lange <klange@toaruos.org>
2021-03-08 14:48:59 +0900

    Really make sure we're breaking out of the right loop in list.extend() on exception

--- src/obj_list.c
+++ src/obj_list.c
@@ -125,7 +125,7 @@ KRK_METHOD(list,__repr__,{
 			for (size_t i = 0; i < counter; ++i) { \
 				positionals->values[positionals->count] = indexer; \
 				positionals->count++; \
-				if (krk_currentThread.flags & KRK_THREAD_HAS_EXCEPTION) break; \
+				if (krk_currentThread.flags & KRK_THREAD_HAS_EXCEPTION) return NONE_VAL(); \
 			} \
 		} while (0)
 KRK_METHOD(list,extend,{
