477a0978c06a10771ad2e5ee057da252609f9bb7
K. Lange <klange@toaruos.org>
2021-04-14 18:02:30 +0900

    Add a quick timeit.timeit implementation as a C module

---
+++ src/module_timeit.c
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <kuroko/vm.h>
+#include <kuroko/value.h>
+#include <kuroko/object.h>
+#include <kuroko/util.h>
+
+KRK_FUNC(timeit,{
+	KrkValue number;
+	int times = 1000000;
+	if (hasKw && krk_tableGet_fast(AS_DICT(argv[argc]), S("number"), &number)) {
+		if (!IS_INTEGER(number)) return TYPE_ERROR(int,number);
+		times = AS_INTEGER(number);
+	}
+
+	int flagsBefore = vm.globalFlags;
+	vm.globalFlags |= KRK_GLOBAL_GC_PAUSED;
+
+	struct timeval tv_before, tv_after;
+	gettimeofday(&tv_before,NULL);
+	for (krk_integer_type t = 0; t < times; ++t) {
+		krk_callSimple(argv[0],0,0);
+	}
+	gettimeofday(&tv_after,NULL);
+
+	vm.globalFlags = flagsBefore;
+
+	double before = (double)tv_before.tv_sec + (double)tv_before.tv_usec / 1000000.0;
+	double after = (double)tv_after.tv_sec + (double)tv_after.tv_usec / 1000000.0;
+
+	return FLOATING_VAL(after-before);
+})
+
+KrkValue krk_module_onload_timeit(void) {
+	KrkInstance * module = krk_newInstance(vm.baseClasses->moduleClass);
+	krk_push(OBJECT_VAL(module));
+
+	KRK_DOC(module, "@brief Run functions very quickly without loop overhead from the interpreter.");
+
+	BIND_FUNC(module,timeit);
+
+	krk_pop();
+	return OBJECT_VAL(module);
+}
