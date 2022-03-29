dbd1071bc15e1846885231cd659d91281df07e59
K. Lange <klange@toaruos.org>
2022-03-29 10:17:36 +0900

    Internalize random's rng, seed it with more bits

--- src/modules/module_random.c
+++ src/modules/module_random.c
@@ -6,13 +6,36 @@
  */
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 #include <kuroko/vm.h>
 #include <kuroko/util.h>
+
+static uint32_t x = 123456789;
+static uint32_t y = 362436069;
+static uint32_t z = 521288629;
+static uint32_t w = 88675123;
+
+static int _rand(void) {
+	uint32_t t;
+
+	t = x ^ (x << 11);
+	x = y; y = z; z = w;
+	w = w ^ (w >> 19) ^ t ^ (t >> 8);
+
+	return (w & RAND_MAX);
+}
+
+void _srand(unsigned int seed) {
+	x = 123456789  ^ (seed << 16) ^ (seed >> 16);
+	y = 362436069;
+	z = 521288629;
+	w = 88675123;
+}
 
 KRK_FUNC(random,{
 	FUNCTION_TAKES_NONE();
 
-	double r = (double)rand() / (double)(RAND_MAX);
+	double r = (double)_rand() / (double)(RAND_MAX);
 
 	return FLOATING_VAL(r);
 })
@@ -25,10 +48,13 @@ KRK_FUNC(seed,{
 		CHECK_ARG(0,int,krk_integer_type,_seed);
 		seed = _seed;
 	} else {
-		seed = time(NULL);
+		struct timeval tv;
+		gettimeofday(&tv,NULL);
+
+		seed = tv.tv_sec ^ tv.tv_usec;
 	}
 
-	srand(seed);
+	_srand(seed);
 })
 
 KrkValue krk_module_onload_random(void) {
@@ -39,6 +65,8 @@ KrkValue krk_module_onload_random(void) {
 
 	BIND_FUNC(module, random);
 	BIND_FUNC(module, seed);
+
+	FUNC_NAME(krk,seed)(0,NULL,0);
 
 	return krk_pop();
 }
