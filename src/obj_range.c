3601411f5444a1ab99d1fedc13706409cdc824e2
K. Lange <klange@toaruos.org>
2022-05-26 20:41:39 +0900

    Support step value in range()

--- src/obj_range.c
+++ src/obj_range.c
@@ -14,6 +14,7 @@ struct Range {
 	KrkInstance inst;
 	krk_integer_type min;
 	krk_integer_type max;
+	krk_integer_type step;
 };
 static KrkClass * range = NULL;
 #define IS_range(o)   (krk_isInstanceOf(o,range))
@@ -23,6 +24,7 @@ struct RangeIterator {
 	KrkInstance inst;
 	krk_integer_type i;
 	krk_integer_type max;
+	krk_integer_type step;
 };
 static KrkClass * rangeiterator = NULL;
 #define IS_rangeiterator(o) (krk_isInstanceOf(o,rangeiterator))
@@ -35,8 +37,9 @@ FUNC_SIG(rangeiterator,__init__);
 
 KRK_METHOD(range,__init__,{
 	METHOD_TAKES_AT_LEAST(1);
-	METHOD_TAKES_AT_MOST(2);
+	METHOD_TAKES_AT_MOST(3);
 	self->min = 0;
+	self->step = 1;
 	if (argc == 2) {
 		CHECK_ARG(1,int,krk_integer_type,_max);
 		self->max = _max;
@@ -45,6 +48,13 @@ KRK_METHOD(range,__init__,{
 		CHECK_ARG(2,int,krk_integer_type,_max);
 		self->min = _min;
 		self->max = _max;
+		if (argc == 4) {
+			CHECK_ARG(3,int,krk_integer_type,_step);
+			if (_step == 0) {
+				return krk_runtimeError(vm.exceptions->valueError, "range() arg 3 must not be zero");
+			}
+			self->step = _step;
+		}
 	}
 	return argv[0];
 })
@@ -53,8 +63,15 @@ KRK_METHOD(range,__repr__,{
 	METHOD_TAKES_NONE();
 	krk_integer_type min = self->min;
 	krk_integer_type max = self->max;
+	krk_integer_type step = self->step;
+
 	char tmp[1024];
-	size_t len = snprintf(tmp,1024,"range(" PRIkrk_int "," PRIkrk_int ")", min, max);
+	size_t len;
+	if (step == 1) {
+		len = snprintf(tmp,1024,"range(" PRIkrk_int "," PRIkrk_int ")", min, max);
+	} else {
+		len = snprintf(tmp,1024,"range(" PRIkrk_int "," PRIkrk_int "," PRIkrk_int ")", min, max, step);
+	}
 	return OBJECT_VAL(krk_copyString(tmp,len));
 })
 
@@ -62,9 +79,10 @@ KRK_METHOD(range,__iter__,{
 	KrkInstance * output = krk_newInstance(rangeiterator);
 	krk_integer_type min = self->min;
 	krk_integer_type max = self->max;
+	krk_integer_type step = self->step;
 
 	krk_push(OBJECT_VAL(output));
-	FUNC_NAME(rangeiterator,__init__)(3, (KrkValue[]){krk_peek(0), INTEGER_VAL(min), INTEGER_VAL(max)},0);
+	FUNC_NAME(rangeiterator,__init__)(4, (KrkValue[]){krk_peek(0), INTEGER_VAL(min), INTEGER_VAL(max), INTEGER_VAL(step)},0);
 	krk_pop();
 
 	return OBJECT_VAL(output);
@@ -74,19 +92,23 @@ KRK_METHOD(range,__iter__,{
 #define CURRENT_CTYPE struct RangeIterator *
 
 KRK_METHOD(rangeiterator,__init__,{
-	METHOD_TAKES_EXACTLY(2);
-	self->i = AS_INTEGER(argv[1]);
-	self->max = AS_INTEGER(argv[2]);
+	METHOD_TAKES_EXACTLY(3);
+	CHECK_ARG(1,int,krk_integer_type,i);
+	CHECK_ARG(2,int,krk_integer_type,max);
+	CHECK_ARG(3,int,krk_integer_type,step);
+	self->i = i;
+	self->max = max;
+	self->step = step;
 	return argv[0];
 })
 
 KRK_METHOD(rangeiterator,__call__,{
 	METHOD_TAKES_NONE();
 	krk_integer_type i = self->i;
-	if (i >= self->max) {
+	if (self->step > 0 ? (i >= self->max) : (i <= self->max)) {
 		return argv[0];
 	} else {
-		self->i = i + 1;
+		self->i = i + self->step;
 		return INTEGER_VAL(i);
 	}
 })
