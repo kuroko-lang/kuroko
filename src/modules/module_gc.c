02c6f58f3c148a3e763c277ab3ac965cfff9058e
K. Lange <klange@toaruos.org>
2023-12-14 13:47:23 +0900

    Separate 'gc' into shared module

---
+++ src/modules/module_gc.c
@@ -0,0 +1,41 @@
+#include <kuroko/vm.h>
+#include <kuroko/util.h>
+
+KRK_Function(collect) {
+	FUNCTION_TAKES_NONE();
+	if (&krk_currentThread != vm.threads) return krk_runtimeError(vm.exceptions->valueError, "only the main thread can do that");
+	return INTEGER_VAL(krk_collectGarbage());
+}
+
+KRK_Function(pause) {
+	FUNCTION_TAKES_NONE();
+	vm.globalFlags |= (KRK_GLOBAL_GC_PAUSED);
+	return NONE_VAL();
+}
+
+KRK_Function(resume) {
+	FUNCTION_TAKES_NONE();
+	vm.globalFlags &= ~(KRK_GLOBAL_GC_PAUSED);
+	return NONE_VAL();
+}
+
+KrkValue krk_module_onload_gc(KrkString * runAs) {
+	/**
+	 * gc = module()
+	 *
+	 * Namespace for methods for controlling the garbage collector.
+	 */
+	KrkInstance * module = krk_newInstance(vm.baseClasses->moduleClass);
+	krk_push(OBJECT_VAL(module));
+
+	KRK_DOC(module, "@brief Namespace containing methods for controlling the garbage collector.");
+
+	KRK_DOC(BIND_FUNC(module,collect),
+		"@brief Triggers one cycle of garbage collection.");
+	KRK_DOC(BIND_FUNC(module,pause),
+		"@brief Disables automatic garbage collection until @ref resume is called.");
+	KRK_DOC(BIND_FUNC(module,resume),
+		"@brief Re-enable automatic garbage collection after it was stopped by @ref pause ");
+
+	return krk_pop();
+}
